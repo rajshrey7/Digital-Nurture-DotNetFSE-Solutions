6707ed48ff3841bdcb3138282afb645f7a734249
Shreyansh Raj <rshreyansh759@gmail.com>
2026-06-02 20:20:15 +0530

    Complete jQuery Exercise 2 - Element selection

---
+++ 05_jQuery/Exercise02_Selectors/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("h1").text(
+        "Welcome to the Community Portal"
+    );
+
+    $("#hideBtn").click(function () {
+
+        $("#para2").hide();
+
+    });
+
+});
